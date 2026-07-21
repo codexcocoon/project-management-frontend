4190f4634db2533f94db308cf84b671cfed19bc1
codexcocoon <codexcocoon@gmail.com>
2026-07-21 22:26:26 +0600

    Going Live

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://lara-react-full.test/api",
+  baseURL: "https://pro-manage.arkangroupbd.com/api",
 });
 export default api;
